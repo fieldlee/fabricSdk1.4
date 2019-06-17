8191af69d905e3a56ee10cd0432a5d1e32590e08
fieldlee <ldpfieldlee@gmail.com>
2019-06-17 14:57:45 +0800

    fix

--- app/event.js
+++ app/event.js
@@ -10,9 +10,25 @@ var connectionProfile = hfc.getConfigSetting('connect_profile');
 
 const gateway = new Gateway();
 const wallet = new FileSystemWallet('/var/wallet');
+
+
+const adminExists = await wallet.exists('admin');
+if (adminExists) {
+    console.log('An identity for the admin user "admin" already exists in the wallet');
+}
+
+var client = await helper.getClientForOrg("mmOrg");
+var ca = client.getCertificateAuthority();
+// Enroll the admin user, and import the new identity into the wallet.
+const enrollment = await ca.enroll({ enrollmentID: 'admin', enrollmentSecret: 'adminpw' });
+const identity = X509WalletMixin.createIdentity('mmOrg', enrollment.certificate, enrollment.key.toBytes());
+await wallet.import('admin', identity);
+console.log('Successfully enrolled admin user "admin" and imported it into the wallet');
+
+
 const gatewayOptions = {
     wallet: wallet, 
-    identity: "fieldlee"
+    identity: "admin"
 };
 
 gateway.connect(connectionProfile, gatewayOptions).then(()=>{
